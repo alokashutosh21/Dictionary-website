5870d6988a02259a0f1c55bb686b4106b5279396
Alok <ashutodang2004@gmail.com>
2021-08-14 22:14:08 +0530

    made it responsive

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,29 @@ const submitBtn = document.getElementById("submitBtn")
 const searchWord = document.getElementById("searchWord")
 const result = document.getElementById("mean_results")
 const example = document.getElementById("mean_ex")
+const output_box = document.getElementById("output_box")
+const burger = document.getElementById("burger")
+const cross = document.getElementById("cross")
 const output = document.querySelector(".dic_name")
 const result_box = document.querySelector(".result_box")
 
 
+burger.addEventListener("click",()=>{    
+    document.getElementById("main_nav").style.visibility = "visible";
+    document.getElementById("logo").style.visibility = "hidden";    
+    document.getElementById("cross").style.visibility = "visible"
+    document.getElementById("burger").style.visibility = "hidden"
+})
+cross.addEventListener("click",()=>{
+    document.getElementById("main_nav").style.visibility = "hidden";
+    document.getElementById("logo").style.visibility = "visible";    
+    document.getElementById("burger").style.visibility = "visible"
+    document.getElementById("cross").style.visibility = "hidden"
+})
+
 result_box.classList.add("dic_hide")
+
+
 getmeaning = async (event)=>{
     event.preventDefault()
     
@@ -25,10 +43,10 @@ getmeaning = async (event)=>{
             example.innerText = data[0].meanings[0].definitions[0].example;
 
             result_box.classList.remove("dic_hide")
-            output.classList.add("dic_hide")
+            output_box.classList.add("dic_hide")
             
         } catch {
-            output.classList.remove("dic_hide")
+            output_box.classList.remove("dic_hide")
             result_box.classList.add("dic_hide")
             output.innerHTML = "Search a VALID Word"
         }
